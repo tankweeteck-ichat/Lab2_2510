cc7db58e2189d57fc6e94fb4bde64cbdbbb94009
ktsp <rommel6853@yahoo.com>
2025-05-12 18:09:56 +0800

    Added return integer to bmi.py

--- bmi.py
+++ bmi.py
@@ -7,10 +7,13 @@ def calculate_bmi(height, weight):
 
     if bmi < 18.5:
         print("Under weight")
+        return -1
     elif bmi > 25.0:
         print("Over weight")
+        return 1
     else:
         print("Normal weight")
+        return 0
 
 
 print("--------------------------------")
